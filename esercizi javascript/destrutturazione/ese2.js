34f067f08c8ce8ff5f130090dad1695b4e2d30a5
chiara <chiarapaluzzi34@gmail.com>
2023-11-29 16:35:17 +0100

    destrutturazione

---
+++ esercizi javascript/destrutturazione/ese2.js
@@ -0,0 +1,11 @@
+//Dato uno studente oggetto nidificato con informazioni sulle proprietà e voti, utilizza la destrutturazione dell'oggetto per creare variabili nome, età e voto che contengano i valori dell'oggetto nidificato.
+const student = {
+    info: { name: "Bob", age: 25 },
+    grades: { math: 95, science: 89 }
+  };
+  const { info: { name, age }, grades: { math, science } } = student;
+  console.log(student)
+  console.log(name)
+  console.log(age)
+  console.log(math)
+  console.log(science)
